063cb3c3a0c94d81eed55f49085348265e922610
somaan02 <xre20jgu@uea.ac.uk>
2026-01-14 13:40:07 +0000

    Implemented threading logic with MVars for message coordination - Added userThread function that recursively sends message until limit reached - Created messageCounter to track total messages - Created userCounter MVars to track received messages - Implemented getRandomMessage helper with message options - Implemented getRandomUser helper to select random recipient - Added delays to simulate realistic user behaviour - Thread stops when 100 messages reached across all users

--- social-network/app/Main.hs
+++ social-network/app/Main.hs
@@ -1,6 +1,82 @@
-module Main (main) where
+module Main where
 
 import Lib
+import Control.Concurrent
+import System.Random
 
+-- / Main entry point of the social network application
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Welcome to the Somaans' Social Network Platform!"
+    putStrLn "Creating users..."
+
+    let users = createUsers 10 -- Create 10 users
+
+    -- Print the users
+    mapM_ print users
+    putStrLn "\n Initialising..."
+
+
+    -- Creating MVar to count total messages sent
+    messageCounter <- newMVar 0
+
+    -- Create MVar for each user to track their received messages
+    -- One MVar per user, count starts at 0
+    userCounter <- mapM (\_ -> newMVar 0) users
+
+    putStrLn "Setup completed!"
+
+-- / Generates random message content 
+getRandomMessage :: IO String
+getRandomMessage = do
+    r <- randomRIO (0, 5) :: IO Int
+    return $ messages !! r
+    where
+        messages = ["Hi", "Morning", "How are you?", "What are you up to?", "Coffee?", "Fancy the boozer later?"]
+
+-- / Selects a user at random from the list
+getRandomUser :: User -> [User] -> IO User
+getRandomUser current allUsers = do
+    let otherUsers = filter (/= current) allUsers
+    idx <- randomRIO (0, length otherUsers - 1)
+    return $ otherUsers !! idx
+
+-- / Threading behaviour, runs a loop sending messages
+userThread :: User -> [User] -> MVar Int -> [MVar Int] -> IO ()
+userThread currentUser allUsers messageCounter userCounter = do
+    -- checker to see if 100 messages have been sent
+    count <- takeMVar messageCounter
+
+    if count >= 100
+        then do
+            -- End condition met, output count and exit
+            putMVar messageCounter count
+            return()
+        else do
+            -- Continue to send messages
+            putMVar messageCounter count
+
+            -- Initialise random delay (simulate a texting chain)
+            delay <- randomRIO (100000, 1000000)
+            threadDelay delay
+
+            -- Select a random user to send a message to
+            receiver <- getRandomUser currentUser allUsers
+
+            -- Get random message
+            msg <- getRandomMessage
+
+            -- return the message
+            putStrLn $ username currentUser ++ " -> " ++ username receiver ++ ": " ++ msg
+
+            -- increment to the message counter
+            _ <- takeMVar messageCounter
+            putMVar messageCounter (count + 1)
+
+            -- increment the receiver's message count
+            let receiverIdx = userId receiver - 1
+            oldCount <- takeMVar (userCounter !! receiverIdx)
+            putMVar (userCounter !! receiverIdx) (oldCount + 1)
+
+            -- recursively call the userThread to continue sending messages
+            userThread currentUser allUsers messageCounter userCounter
--- social-network/src/Lib.hs
+++ social-network/src/Lib.hs
@@ -1,6 +1,25 @@
+-- / Library modules containing all social network commands
 module Lib
-    ( someFunc
+    ( User(..)
+    , Message(..)
+    , createUsers
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- / Represents a user in the social network
+-- Contains username and unique userID
+data User = User
+    { username :: String    -- ^ Users name 
+    , userId :: Int      -- ^ Unique user identifier
+    } deriving (Show, Eq)
+
+-- /  Represents a message between users
+data Message = Message
+    { sender :: String  -- ^ Sender's username
+    , receiver :: String  -- ^ Receiver's username
+    , messageContent :: String  -- ^ Content of the message
+    } deriving (Show)
+
+-- / Creates a list of n users
+-- "User1, User2 ..."
+createUsers :: Int -> [User]
+createUsers n = [User ("User" ++ show i) i | i <- [1..n]] -- List comprehension to n number of users
